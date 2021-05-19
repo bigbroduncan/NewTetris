3ec17f826d5b370136f8249b2497e32824508559
Kevin <49698045+KevinO313@users.noreply.github.com>
2021-05-19 08:33:10 -0500

    Add files via upload
    Git bash was not working for me, so I have to upload my code directly.

---
+++ NewTetris-main/NewTetris-main/project/NewTetris/FrmMain.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace NewTetris {
+  partial class FrmMain {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lblPlayingField = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tmrCurrentPieceFall = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPlayingField
+            // 
+            this.lblPlayingField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblPlayingField.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPlayingField.Location = new System.Drawing.Point(397, 89);
+            this.lblPlayingField.Name = "lblPlayingField";
+            this.lblPlayingField.Size = new System.Drawing.Size(450, 660);
+            this.lblPlayingField.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(48, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Level:";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblLevel.Location = new System.Drawing.Point(145, 165);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(42, 25);
+            this.lblLevel.TabIndex = 10;
+            this.lblLevel.Text = "XX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(48, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Next Block:";
+            // 
+            // tmrCurrentPieceFall
+            // 
+            this.tmrCurrentPieceFall.Enabled = true;
+            this.tmrCurrentPieceFall.Interval = 500;
+            this.tmrCurrentPieceFall.Tick += new System.EventHandler(this.tmrCurrentPieceFall_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.Location = new System.Drawing.Point(186, 390);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 107);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox3.Location = new System.Drawing.Point(186, 231);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(120, 107);
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(46, 390);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 24);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Stored Block:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1183, 803);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPlayingField);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FrmMain_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+    private System.Windows.Forms.Label lblPlayingField;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label lblLevel;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Timer tmrCurrentPieceFall;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NewTetris-main/NewTetris-main/project/NewTetris/FrmMain.cs
@@ -0,0 +1,87 @@
+﻿using NewTetris.Properties;
+using NewTetris_Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewTetris {
+  public partial class FrmMain : Form {
+    public Game game;
+    private bool d_was_pressed = false;
+    private int defSpeed;
+
+    public FrmMain() {
+      InitializeComponent();
+      Game.imgPiece2 = Resources.block_piece2;
+      Game.imgPiece = Resources.block_piece;
+      game = new Game();
+      Game.field = lblPlayingField;
+      Game.storage = pictureBox1;
+      Game.next = pictureBox3;
+      Game.imgLine = Resources.line_shape;
+      Game.imgSquare = Resources.square_shape;
+      Game.imgL = Resources.L_shape;
+      Game.imgR = Resources.R_shape;
+      Game.imgZ = Resources.Z_shape;
+      Game.imgT = Resources.T_shape;
+      Game.imgRZ = Resources.RZ_shape;
+      game.NextShape();
+    }
+
+    private void tmrCurrentPieceFall_Tick(object sender, EventArgs e) {
+      if (Game.curShape != null) {
+        if (!Game.curShape.TryMoveDown()) {
+          Game.curShape.DissolveIntoField();
+          Game.curShape.ErasePiecePic(); //TESTING
+          Game.curShape = null;
+          game.NextShape();
+        }
+      }
+    }
+
+    private void FrmMain_KeyUp(object sender, KeyEventArgs e) {
+      if ((d_was_pressed) && (e.KeyCode != Keys.Down)) // ret speed to norm releasing down
+            {
+                d_was_pressed = false;
+                this.tmrCurrentPieceFall.Interval = defSpeed;
+            }
+      else if (e.KeyCode == Keys.Left) {
+        Game.curShape.TryMoveLeft();
+      }
+      else if (e.KeyCode == Keys.Right) {
+        Game.curShape.TryMoveRight();
+      }
+      else if (e.KeyCode == Keys.Up)
+            {
+                Game.curShape.PlaceUnder();
+            }
+      else if (!(d_was_pressed) && (e.KeyCode == Keys.Down)) // inc speed pressing down
+            {
+                d_was_pressed = true;
+                defSpeed = this.tmrCurrentPieceFall.Interval;
+                this.tmrCurrentPieceFall.Interval = 100;
+            }
+      else if (e.KeyCode == Keys.Space) // store shapes
+            {
+                game.StoreShape();
+            }
+      else if (e.KeyCode == Keys.Z) {
+        Game.curShape.RotateCCW();
+      }
+      else if (e.KeyCode == Keys.X) {
+        Game.curShape.RotateCW();
+      }
+    }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+  }
+}
